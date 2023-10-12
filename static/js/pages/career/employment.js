7840efda322f00d96ea48203c96e49494f69b78c
Irene-Busah <i.busah@alustudent.com>
2023-10-12 20:31:47 +0200

    Fixing single job popup

--- static/js/pages/career/employment.js
+++ static/js/pages/career/employment.js
@@ -27,44 +27,75 @@
 // closeModalBtn.addEventListener('click', closeModal);
 // modalOverlay.addEventListener('click', closeModal);
 
-const cardWrapper = document.querySelector('.job-listing');
-const closeModalBtn = document.querySelector('#close-modal');
-const modal = document.querySelector('.modal');
-const modalOverlay = document.querySelector('.modal-overlay');
-const modalJobTitle = modal.querySelector("#job_title");
-const modalJobCode = modal.querySelector('#job_code');
-const modalCompName = modal.querySelector('#company_name');
-const modalLocation = modal.querySelector('#location');
-const modalDescription = modal.querySelector('#job_description');
-const modalJobType = modal.querySelector('#job_type');
-const modalJobRequirement = modal.querySelector('#job_requirements');
-const closeModal = () => {
-    modal.style.display = 'none';
-    modalOverlay.style.display = 'none';
-}
+// const cardWrapper = document.querySelector('.job-listing');
+// const closeModalBtn = document.querySelector('#close-modal');
+// const modal = document.querySelector('.modal');
+// const modalOverlay = document.querySelector('.modal-overlay');
+// const modalJobTitle = modal.querySelector("#job_title");
+// const modalJobCode = modal.querySelector('#job_code');
+// const modalCompName = modal.querySelector('#company_name');
+// const modalLocation = modal.querySelector('#location');
+// const modalDescription = modal.querySelector('#job_description');
+// const modalJobType = modal.querySelector('#job_type');
+// const modalJobRequirement = modal.querySelector('#job_requirements');
+// const closeModal = () => {
+//     modal.style.display = 'none';
+//     modalOverlay.style.display = 'none';
+// }
+// cardWrapper.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if (e.target.classList.contains('jobs-overlay')) {
+//         modal.style.display = 'block';
+//         modalOverlay.style.display = 'block';
 
-cardWrapper.addEventListener('click', (e) => {
-    const card = e.target.closest('.jobs');
-    if (card) {
-        const jobId = card.getAttribute('data-job-id');
-        const job = findJobById(jobId); // Replace with your logic to find the job by ID
+//         modalJobTitle.textContent = e.target.nextElementSibling.querySelector(".first h1").textContent;
+//         modalCompName.textContent = e.target.nextElementSibling.querySelector(".first h2").textContent;
+//         modalDescription.textContent = e.target.nextElementSibling.querySelector(".first p").textContent;
+//     }
+// })
+// closeModalBtn.addEventListener('click', closeModal);
+// modalOverlay.addEventListener('click', closeModal);
 
-        if (job) {
-            modal.style.display = 'block';
-            modalOverlay.style.display = 'block';
+$(document).ready(function () {
+    const modelOverlay = $('#modal-overlay')
+    // Click event for job listings
+    $('.jobs').on('click', function (e) {
+        e.preventDefault();
 
-            modalJobTitle.textContent = job.title;
-            modalCompName.textContent = `${job.companyName} - ${job.jobLocation.city}, ${job.jobLocation.state}, ${job.jobLocation.zip}`;
-            modalDescription.textContent = job.description;
-        }
-    }
+        // Extract the job ID from the clicked job listing
+        // let jobId = $(this).attr('data-job-id');
+
+        // Use AJAX to fetch job details based on the job ID
+        $.ajax({
+            url: `/job/1996739/`,
+            method: 'GET',
+            success: function (data) {
+                if (data.error) {
+                    console.error('Error:', data.error);
+                } else {
+                    // Populate the modal with job details
+                    $('#job_title').text(data.title);
+                    $('#job_code').text(data.jobCode);
+                    $('#company_name').text(`${data.companyName} - ${data.jobLocation.city}, ${data.jobLocation.state}, ${data.jobLocation.zip}`);
+                    $('#job_type').text(data.jobType);
+                    $('#job_description').html(data.description);
+                    $('#job_requirements').html(data.requirements);
+                    $('#apply').attr('href', data.applyUrl);
+
+                    // Display the modal
+                    modelOverlay.show()
+                    $('.modal').show();
+                }
+            },
+            error: function (error) {
+                console.error('Error:', error);
+            }
+        });
+    });
+
+    // Close modal
+    $('#close-modal').on('click', function () {
+        modelOverlay.hide()
+        $('.modal').hide();
+    });
 });
-
-closeModalBtn.addEventListener('click', closeModal);
-modalOverlay.addEventListener('click', closeModal);
-
-// Replace this function with your logic to find the job by ID
-function findJobById(jobId) {
-    // Implement logic to find the job by ID in your job_list
-    return jobListData.find(job => job.jobId === jobId);
-}
--- webApp/views/career.py
+++ webApp/views/career.py
@@ -19,7 +19,7 @@ def getJobs(request):
 
 def jobList(request):
     job_list = getJobs(request)
-    # print(job_list)
+    
     context = {"job_list": job_list}
     return render(request, 'career/career.html', context)
 
@@ -28,18 +28,17 @@ def getJobDetail(request, id):
     try:
         api_url = "https://recruiting.paylocity.com/recruiting/v2/api/feed/jobs/c3d7ea2b-cf6b-479a-a7f5-79ef063bb61f"
         
-        # Send a GET request to the API
         response = requests.get(api_url)
 
-        # Check the response status code
         if response.status_code == 200:
             data = response.json()
             job_list = data.get('jobs', [])
 
             job_detail = next((job for job in job_list if job['jobId'] == int(id)), None)
+            print(job_detail)
 
             if job_detail:
-                return render(request, 'career/job_detail.html', {'job_detail': job_detail})
+                return JsonResponse(job_detail)
             else:
                 return HttpResponse(f"Job not found (ID: {id})", content_type="text/plain", status=404)
         else:
@@ -52,7 +51,6 @@ def getJobDetail(request, id):
 
 def filter_jobTypes(request):
     if request.method == 'GET':
-        
         selected_filter = request.GET.getlist('jobTypes[]')
         print(selected_filter)
         
@@ -66,11 +64,11 @@ def filter_jobTypes(request):
                 # Check if any checkboxes are selected
                 if not selected_filter:
                     # If no checkboxes are selected, return all jobs
-                    serialized_jobs = [{'title': job['title'], 'description': job['description'], 'companyName': job['companyName'], 'zip': job['jobLocation']['zip'], 'city': job['jobLocation']['city'], 'state': job['jobLocation']['state']} for job in all_jobs]
+                    serialized_jobs = [{'jobId': job['jobId'],'title': job['title'], 'description': job['description'], 'companyName': job['companyName'], 'zip': job['jobLocation']['zip'], 'city': job['jobLocation']['city'], 'state': job['jobLocation']['state']} for job in all_jobs]
                 else:
                     # Filter jobs based on selected checkboxes
                     filtered_jobs = [job for job in all_jobs if job.get('jobTypes') in selected_filter]
-                    serialized_jobs = [{'title': job['title'], 'description': job['description'], 'companyName': job['companyName'], 'zip': job['jobLocation']['zip'], 'city': job['jobLocation']['city'], 'state': job['jobLocation']['state']} for job in filtered_jobs]
+                    serialized_jobs = [{'jobId': job['jobId'], 'title': job['title'], 'description': job['description'], 'companyName': job['companyName'], 'zip': job['jobLocation']['zip'], 'city': job['jobLocation']['city'], 'state': job['jobLocation']['state']} for job in filtered_jobs]
                 
                 return JsonResponse({'jobs': serialized_jobs})
             else:
@@ -80,14 +78,13 @@ def filter_jobTypes(request):
             return JsonResponse({'error': str(e)}, status=500)
 
 
-
+# from datetime import datetime, timedelta
 
 def filter_recent_and_past_jobs(request):
     if request.method == 'GET':
         # Get the values of the recent_jobs and saved_jobs checkboxes
         recent_jobs = request.GET.get('recent_jobs')
         saved_jobs = request.GET.get('saved_jobs')
-        print(recent_jobs)
 
         api_url = "https://recruiting.paylocity.com/recruiting/v2/api/feed/jobs/c3d7ea2b-cf6b-479a-a7f5-79ef063bb61f"
 
@@ -97,15 +94,21 @@ def filter_recent_and_past_jobs(request):
                 all_jobs = response.json().get('jobs', [])
 
                 if recent_jobs:
+                    # Filter jobs for most recent (e.g., published within the last 7 days)
                     threshold_date = datetime.now() - timedelta(days=7)
-                    filtered_jobs = [job for job in all_jobs if datetime.fromisoformat(job["publishedDate"].split('.')[0]) >= threshold_date ]
-                    # print(filtered_jobs)
+                    filtered_jobs = [
+                        job for job in all_jobs
+                        if datetime.fromisoformat(job["publishedDate"].split('.')[0]) >= threshold_date
+                    ]
                 elif saved_jobs:
+                    # Filter jobs for saved (e.g., published more than 7 days ago)
                     threshold_date = datetime.now() - timedelta(days=7)
-                    filtered_jobs = [job for job in all_jobs if datetime.fromisoformat(job["publishedDate"].split('.')[0]) < threshold_date]
-                    # print(filtered_jobs)
+                    filtered_jobs = [
+                        job for job in all_jobs
+                        if datetime.fromisoformat(job["publishedDate"].split('.')[0]) <= threshold_date
+                    ]
                 else:
-                    
+                    # If neither checkbox is checked, return all jobs
                     filtered_jobs = all_jobs
 
                 serialized_jobs = [
