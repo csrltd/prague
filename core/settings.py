8072301223ca606a0e19c4cc0fa2da016cc8f606
csrolivier <okarera@compstaffing.com>
2024-06-20 17:40:41 +0200

    Refining the responsiveness

--- core/settings.py
+++ core/settings.py
@@ -114,7 +114,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
