999f07f6c768a1f689e2572e6476adf6e65532c2
csrltd <salphonse@compstaffing.com>
2024-06-20 10:19:25 +0200

    prepating to deploy

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
