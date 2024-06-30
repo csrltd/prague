286807755bfef2c9aae4f55c3a864a30b29bfb55
csrltd <salphonse@compstaffing.com>
2024-06-30 11:16:46 +0200

    added more team members on prague

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,9 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = (
+    "l7*fv-@wadq8@l^d*huak+!$((ww8)d9p@$-#v90vafukuo@v9"  # os.getenv("SECRET_KEY")
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
